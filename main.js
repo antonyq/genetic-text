eec0ac725758fbfdd4ac72d463e2d42337c613fb
antonyq <antonyq@ukr.net>
2017-07-03 20:54:50 +0300

    master

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
-const START_POPULATION_COUNT = 100;
-const GENERATIONS_LIMIT = 100;
-const FRACTION = 0.7;
+const ALPHABET = '123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz';
+const DEFAULT_SENTENCE = "Evolve or dissolve";
+const POPULATION_COUNT = 100;
+const GENERATIONS_LIMIT = 1000;
+const FRACTION = 0.9;
 
-const randInt = (min, max) => min + Math.ceil(Math.random() * (max - min));
+const randInt = (min, max) => min + Math.round(Math.random() * (max - min));
 const randChar = (alphabet) => alphabet[randInt(0, alphabet.length - 1)];
 
 window.onload = () => {
-    let word = prompt('Word:', 'word');
-    let population = randWords(START_POPULATION_COUNT, word.length);
+    let word = prompt('Sentence:', DEFAULT_SENTENCE);
+    let population = randWords(POPULATION_COUNT, word.length);
     evolution(population, word);
 }
 
@@ -16,33 +17,39 @@ window.onload = () => {
 
 // EVOLUTION //
 function evolution (population, targetWord) {
+    let dataTable = [];
     for (let i = 0; i < GENERATIONS_LIMIT; i++) {
         if (population.indexOf(targetWord) == -1) {
             let offsprings = croossbreeding(JSON.parse(JSON.stringify(population)));
             population = population.concat(offsprings);
-            population.push(targetWord);
             population = selection(population, targetWord);
-            console.log(healthLevel(population));
+            dataTable.push({
+                'leader': population[0],
+                'fitness': healthLevel(population, targetWord)
+            });
         } else break;
     }
+
+    console.log(`%c ${targetWord}`, 'color: #4AD');
+    console.table(dataTable);
 }
 
 function fitness (word, targetWord) {
     let values = word.split('').map((char, index) => (char == targetWord[index]) ? 1 : 0);
-    return values.reduce((a, b) => a + b);
+    return values.reduce((a, b) => a + b) / targetWord.length;
 }
 
 function selection (population, targetWord) {
     let compare = (a, b) => {
         if (fitness(a, targetWord) < fitness(b, targetWord))
-            return -1;
-        if (fitness(a, targetWord) > fitness(b, targetWord))
             return 1;
+        if (fitness(a, targetWord) > fitness(b, targetWord))
+            return -1;
         return 0;
     }
 
     population = population.sort(compare);
-    population = population.filter((item, index, arr) => index/arr.length < FRACTION);
+    population = population.filter((item, index, arr) => index < POPULATION_COUNT * FRACTION);
     return population;
 }
 
@@ -58,30 +65,24 @@ function croossbreeding (parents) {
 
 function crossover (word1, word2) {
     let childWord = '',
-        separatorIndex = randInt(0, word1.length),
-        crossoverIndex = randInt(0, 1);
+        parents = [word1, word2];
     for (let i = 0; i < word1.length; i++) {
-        if (i == separatorIndex) {
-            childWord += ((crossoverIndex) ? word1[i] : word2[i]) + ((1 - crossoverIndex) ? word2[i] : word1[i]);
-            i++;
-        } else {
-            childWord += word1[i];
-        }
+        let parent = randInt(0, 1);
+        childWord += parents[parent][i];
     }
-    console.log(word1, word2, childWord);
     return childWord;
 }
 
 function mutation (word) {
-    let pointer = randInt(0, word.length - 1);
+    let pointer = randInt(0, word.length);
     return word.split('').map((char, index) => index == pointer ? randChar(ALPHABET) : char).join('');
 }
 
-function healthLevel (population) {
-    let personalHealth = population.map(fitness);
+function healthLevel (population, targetWord) {
+    let personalHealth = population.map((word) => fitness(word, targetWord));
     let sum = personalHealth.reduce((a, b) => a + b);
     let avg = sum / personalHealth.length;
-    return avg;
+    return parseFloat(avg.toFixed(2));
 }
 // EVOLUTION END //
 
